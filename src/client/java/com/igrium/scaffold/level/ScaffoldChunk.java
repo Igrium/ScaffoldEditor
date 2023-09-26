cc02ddd1352d5255df12e4764f98ac7d0298b7b6
Sam54123 <thesam54123@gmail.com>
2023-09-26 12:30:26 -0700

    started world system (for real this time)

--- src/client/java/com/igrium/scaffold/level/ScaffoldChunk.java
+++ src/client/java/com/igrium/scaffold/level/ScaffoldChunk.java
@@ -1,5 +1,6 @@
 package com.igrium.scaffold.level;
 
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3ic;
 
 import net.minecraft.block.BlockState;
@@ -11,11 +12,38 @@ public class ScaffoldChunk {
     // Because Java stores by reference, we can directly store blockstates.
     private final BlockState[][][] blocks = new BlockState[16][16][16];
 
-    public BlockState blockAt(int x, int y, int z) {
+    /**
+     * Get the block at a given position.
+     * @param x Local X.
+     * @param y Local Y.
+     * @param z Local Z.
+     * @return The block. Null if there is none.
+     */
+    @Nullable
+    public BlockState blockAt(int x, int y, int z) throws IndexOutOfBoundsException {
         return blocks[x][y][z];
     }
 
-    public BlockState blockAt(Vector3ic pos) {
+    /**
+     * Get the block at as given position.
+     * @param pos Local position.
+     * @return The block. Null if there is none.
+     */
+    public BlockState blockAt(Vector3ic pos) throws IndexOutOfBoundsException {
         return blockAt(pos.x(), pos.y(), pos.z());
+    }
+
+    /**
+     * Set the block at a given position.
+     * @param x Local X
+     * @param y Local Y
+     * @param z Local Z
+     * @param block The block to set.
+     * @return The old block.
+     */
+    public BlockState setBlock(int x, int y, int z, BlockState block) {
+        BlockState old = blocks[x][y][z];
+        blocks[x][y][z] = block;
+        return old;
     }
 }
--- src/client/java/com/igrium/scaffold/level/ScaffoldWorld.java
+++ src/client/java/com/igrium/scaffold/level/ScaffoldWorld.java
@@ -6,6 +6,10 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3i;
+import org.joml.Vector3ic;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.ChunkSectionPos;
 
 /**
  * The world that Scaffold uses when compiling levels. Note: this is the
@@ -17,7 +21,34 @@ public class ScaffoldWorld {
     @NotNull
     protected Map<Vector3i, ScaffoldChunk> chunks = new HashMap<>();
 
-    // public ScaffoldChunk chunkAt(int x, int y, int z) {
-    //     chunks.get();
-    // }
+    @Nullable
+    public ScaffoldChunk chunkAt(int x, int y, int z) {
+        return chunks.get(new Vector3i(x, y, z));
+    }
+
+    @Nullable
+    public final ScaffoldChunk chunkAt(Vector3ic pos) {
+        return chunkAt(pos.x(), pos.y(), pos.z());
+    }
+
+    @Nullable
+    public BlockState blockAt(int x, int y, int z) {
+        int chunkX = ChunkSectionPos.getSectionCoord(x);
+        int chunkY = ChunkSectionPos.getSectionCoord(y);
+        int chunkZ = ChunkSectionPos.getSectionCoord(z);
+
+        ScaffoldChunk chunk = chunkAt(chunkX, chunkY, chunkZ);
+        if (chunk == null) return null;
+
+        int localX = x & 0xF;
+        int localY = y & 0xF;
+        int localZ = z & 0xF;
+
+        return chunk.blockAt(localX, localY, localZ);
+    }
+
+    @Nullable
+    public final BlockState blockAt(Vector3ic pos) {
+        return blockAt(pos.x(), pos.y(), pos.z());
+    }
 }
